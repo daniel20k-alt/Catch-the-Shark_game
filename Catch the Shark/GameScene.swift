cf14c0a0e79d96829dbc02f52b500cebebb0eeda
Testing_author <testing_beta@mail.com>
2020-08-03 22:47:17 +0300

    Creating path which the user has tapped along, by having each of the swipes being stored.

--- Catch the Shark/GameScene.swift
+++ Catch the Shark/GameScene.swift
@@ -24,6 +24,8 @@ class GameScene: SKScene {
     var activeSlice1: SKShapeNode!
     var activeSlice2: SKShapeNode!
     
+    var activeSlicePoints = [CGPoint]()
+    
     override func didMove(to view: SKView) {
         
         let background = SKSpriteNode(imageNamed: "sea_background")
@@ -56,7 +58,7 @@ class GameScene: SKScene {
     func  createLives() {
         
         for i in 0..<3 {
-            let spriteNode = SKSpriteNode(imageNamed: "LifeGone")
+            let spriteNode = SKSpriteNode(imageNamed: "Life")
             spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
             addChild(spriteNode)
             livesImages.append(spriteNode)
@@ -67,7 +69,7 @@ class GameScene: SKScene {
      
         activeSlice1 = SKShapeNode()
         activeSlice1.zPosition = 2
-        activeSlice1.strokeColor = UIColor(red: 46, green: 134, blue: 222, alpha: 1)
+        activeSlice1.strokeColor = UIColor(red: 72, green: 219, blue: 251, alpha: 1)
         activeSlice1.lineWidth = 9
         
         activeSlice2 = SKShapeNode()
@@ -83,5 +85,57 @@ class GameScene: SKScene {
         rgb(72, 219, 251)
         rgb(84, 160, 255)
          generator at https://flatuicolors.com/palette/ca */
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        activeSlice1.run(SKAction.fadeOut(withDuration: 0.25))
+        activeSlice2.run(SKAction.fadeOut(withDuration: 0.25))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        activeSlicePoints.removeAll(keepingCapacity: true)
+        
+        let location = touch.location(in: self)
+        activeSlicePoints.append(location)
+        
+        redrawActiveSlice()
+        
+        activeSlice1.removeAllActions()
+        activeSlice2.removeAllActions()
+        
+        activeSlice1.alpha = 1
+        activeSlice2.alpha = 2
+    }
+
+    // redrawing the sliced shapes
+    func redrawActiveSlice() {
+        
+        if activeSlicePoints.count < 2 {
+            activeSlice1.path = nil
+            activeSlice2.path = nil
+            return
+        }
+        
+        if activeSlicePoints.count > 12 {
+            activeSlicePoints.removeFirst(activeSlicePoints.count - 12)
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+        
+        for i in 1..<activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+        
+        activeSlice1.path = path.cgPath
+        activeSlice2.path = path.cgPath
     }
 }
